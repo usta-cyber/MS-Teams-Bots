338a613fd46094357dadf80faba196ec60b2398b
Engr. Tajummal Hussain <61576602+usta-cyber@users.noreply.github.com>
2023-08-11 02:43:40 -0700

    Update config.py

--- config.py
+++ config.py
@@ -2,9 +2,8 @@ import os
 
 class DefaultConfig:
     PORT = 3978
-    # APP_ID = os.environ.get("MicrosoftAppId", "")
-    # APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "")
-    APP_ID = os.environ.get("MicrosoftAppId", "49668c1f-c8f6-491a-ae9d-709143924630")
-    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "137c2d23-a2f9-4424-ab7c-3bec4387875f")
+    APP_ID = os.environ.get("MicrosoftAppId", "")
+    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "")
+
 
 #python3.10 -m aiohttp.web -H 0.0.0.0 -P 8000 app:init_func
